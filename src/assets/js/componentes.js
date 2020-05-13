fdde5f9b52b60f2222af579a6d73fbddfb4d3cd3
Jesus <jesus.voluntadheredada.human@gmail.com>
2020-05-12 20:58:25 -0500

    first commit

---
+++ src/assets/js/componentes.js
@@ -0,0 +1,14 @@
+
+import '../css/componentes.css';
+
+export const saludar = ( nombre ) => {
+
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola ${ nombre }`;
+
+    document.body.append( h1 );
+
+}
+
+
+
---
+++ src/assets/js/index.js
@@ -0,0 +1,19 @@
+
+import { saludar } from './assets/js/componentes.js';
+import './styles.css';
+import './assets/css/responsivo.css';
+import './assets/css/all.css';
+import './prefixfree.js';
+
+
+
+
+
+const nombre = 'Jesus';
+
+
+
+
+saludar( nombre );
+
+
